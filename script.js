752e1f1a527d06e3798ce1f1dc7aa37051e9a19a
Fcvssb <jurcy793@gmail.com>
2024-07-08 13:49:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function checkPassword() {
     const result = document.getElementById("result");
 
     if (passwordInput === "Spiffo") {
-        document.getElementById("mainContent").innerHTML = '<h1>OCZEKUJCIE DALEJ</h1>';
+        window.location.href = "https://www.youtube.com/watch?v=SP1FFO_LINK_HERE";
+    } else if (passwordInput === "Patrycusz") {
+        window.location.href = "https://www.youtube.com/watch?v=PATRYCUSZ_LINK_HERE";
     } else if (passwords.includes(passwordInput)) {
         window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     } else {
